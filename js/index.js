7c0c301f2f8349f3a92a4bcbc5cdb1009efddb7c
Krystian Florkowski <krystianf06@outlook.com>
2025-09-21 21:07:21 +0200

    mnozenie-1

--- js/index.js
+++ js/index.js
@@ -1,25 +1,36 @@
-let categoryOne = document.querySelectorAll('.box__item');
+let categoryOne = document.querySelectorAll(".box__item");
 const categoryOneArray = [...categoryOne];
 
-console.log(categoryOne);
+// console.log(categoryOne);
 
-let activeCard = '';
+let activeCard = "";
 
 const clickCard = function (ev) {
-  activeCard = this;
-  if (ev.target.classList[1] === 'hidden') {
-    activeCard.classList.remove('hidden');
-  } else {
-    activeCard.classList.add('color__end');
-  }
+	activeCard = this;
+	if (ev.target.classList[1] === "hidden") {
+		activeCard.classList.remove("hidden");
+	} else {
+		activeCard.classList.add("color__end");
+	}
 };
 
-categoryOneArray.forEach(card => {
-  card.addEventListener('click', clickCard);
+categoryOneArray.forEach((card) => {
+	card.addEventListener("click", clickCard);
 });
 
 setTimeout(() => {
-  categoryOneArray.forEach(card => {
-    card.classList.add('hidden');
-  });
+	categoryOneArray.forEach((card) => {
+		card.classList.add("hidden");
+	});
 }, 10);
+
+const mnozenie = (a, b) => {
+	const c = a * b;
+	const ones = c % 10; // 6
+	const tens = Math.floor(c / 10); // 1
+	// const digits = [tens, ones];
+	console.log(tens, ones);
+	return c;
+};
+
+mnozenie(8, 4);
