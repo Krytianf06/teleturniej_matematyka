fedb29be41f27472113853e742ea24321daebf9f
Krystian Florkowski <krystianf06@outlook.com>
2025-09-29 17:26:12 +0200

    poprawki 1

--- js/index.js
+++ js/index.js
@@ -1,28 +1,30 @@
-let categoryOne = document.querySelectorAll(".box__item");
-const categoryOneArray = [...categoryOne];
+// let categoryOne = document.querySelectorAll(".box__item");
+// const categoryOneArray = [...categoryOne];
 
-// console.log(categoryOne);
+// // console.log(categoryOne);
 
-let activeCard = "";
+// let activeCard = "";
 
-const clickCard = function (ev) {
-	activeCard = this;
-	if (ev.target.classList[1] === "hidden") {
-		activeCard.classList.remove("hidden");
-	} else {
-		activeCard.classList.add("color__end");
-	}
-};
+// const clickCard = function (ev) {
+// 	activeCard = this;
+// 	if (ev.target.classList[1] === "hidden") {
+// 		activeCard.classList.remove("hidden");
+// 	} else {
+// 		activeCard.classList.add("color__end");
+// 	}
+// };
 
-categoryOneArray.forEach((card) => {
-	card.addEventListener("click", clickCard);
-});
+// categoryOneArray.forEach((card) => {
+// 	card.addEventListener("click", clickCard);
+// });
 
-setTimeout(() => {
-	categoryOneArray.forEach((card) => {
-		card.classList.add("hidden");
-	});
-}, 10);
+// setTimeout(() => {
+// 	categoryOneArray.forEach((card) => {
+// 		card.classList.add("hidden");
+// 	});
+// }, 10);
+const Liczba1 = [1, 3, 5, 7];
+const Liczba2 = [3, 4, 5];
 
 const mnozenie = (a, b) => {
 	const c = a * b;
